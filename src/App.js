3096c623afd1c204ed9813ed1d8f94e21c3ca005
Aditya Narayan Jha <adityanarayanjha31@gmail.com>
2022-06-21 17:48:34 +0530

    added error check for fetch

--- src/App.js
+++ src/App.js
@@ -8,42 +8,49 @@ export default function App() {
   const [userData, setUserData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [activeId, setActiveId] = useState(1);
+  const [error, setError] = useState(false);
 
-  const changeActiveId = (id)=>{
+  const changeActiveId = (id) => {
     setActiveId(id)
   }
 
   let url = 'https://602e7c2c4410730017c50b9d.mockapi.io/users'
 
   const fetchData = async (url) => {
-    setLoading(true)
-    let res = await fetch(url);
-    let data = await res.json();
-    setUserData(data);
+    try {
+      setLoading(true)
+      let res = await fetch(url)
+      let data = await res.json();
+      setUserData(data)
+      setError(false)
+    } catch (error) {
+      setError(true)
+    }
     setLoading(false)
   }
 
   useEffect(() => {
     fetchData(url)
     // eslint-disable-next-line
-  },[]);
+  }, []);
 
   return (
     <div>
       <div className='d-flex justify-content-around' style={{ marginTop: '6rem' }}>
-        {loading&&<Spinner/>}
-        {!loading &&
+        {(error && !loading) && <h2 className='text-center'>Some Error Occured</h2>}
+        {loading && <Spinner />}
+        {(!loading && !error)&&
           <>
-            <div className="card" style={{ width: '23rem', height:'100%'}}>
-              <div className='card-header'  style={{backgroundColor:'#8adfbf'}}>
-              <h4 className='text-center'>User List</h4>
+            <div className="card" style={{ width: '23rem', height: '100%' }}>
+              <div className='card-header' style={{ backgroundColor: '#8adfbf' }}>
+                <h4 className='text-center'>User List</h4>
               </div>
               {userData.map((ele) => {
-                return <List key={ele.id} id={ele.id} avatar={ele.avatar} username={ele.profile.username} setActiveId={changeActiveId}/>
+                return <List key={ele.id} id={ele.id} avatar={ele.avatar} username={ele.profile.username} setActiveId={changeActiveId} />
               })}
             </div>
             <div>
-              <UserInfo userData={userData} activeId={activeId} loading={loading}/>
+              <UserInfo userData={userData} activeId={activeId} loading={loading} />
             </div>
           </>
         }
